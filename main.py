f7da8ea186f7129333eeb69426d3c73ee9dfc857
Peyton <146001407+peyton-goodlett@users.noreply.github.com>
2025-03-28 21:50:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-board = [["_", "_", "_"],
-         ["_", "_", "_"],
+import copy
+
+initboard = [["x", "_", "o"],
+         ["x", "o", ""],
          ["_", "_", "_"]]
 player = "x"
 bot = "o"
@@ -10,38 +12,55 @@ def prettyBoard(board):
         print(f"{i+1} " + board[i][0] + "|" + board[i][1] + "|" + board[i][2])
     
 def win(board, player, bot):
-    for row in board:
-        if all(cell == player for cell in row):
-            return True, player
-        elif all(cell == bot for cell in row):
-            return True, bot
-    for col in range(3):
-        if all(board[row][col] == player for row in range(3)):
-            return True, player
-        elif all(board[row][col] == bot for row in range(3)):
-            return True, bot
-        
-    if all(board[i][i] == player for i in range(3)):
-        return True, player
-    if all(board[i][i] == bot for i in range(3)):
-        return True, bot
-    if all(board[i][2 - i] == player for i in range(3)):
-        return True, player
-    if all(board[i][2 - i] == bot for i in range(3)):
-        return True, bot
-    for row in board:
-        r=0
-        if "_" in row:
-            r += 1
-            pass
-    if r == 0:
-        return False, "Tie"
+    for i in range(3):
+        if all(board[i][j] == player for j in range(3)) or all(board[j][i] == player for j in range(3)):
+            return player
+        if all(board[i][j] == bot for j in range(3)) or all(board[j][i] == bot for j in range(3)):
+            return bot
     
-    return False, None
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return player
+    if all(board[i][i] == bot for i in range(3)) or all(board[i][2 - i] == bot for i in range(3)):
+        return bot
     
-def minimax(board, player, bot, turn):
-    copy = board
+    if all(cell != "_" for row in board for cell in row):
+        return "tie"
+    
+    return None
     # look at all 8 possible winning spots and see if its close to filling any 8
     # if we are close to filling one (one spot away) then we will place there to win
     # if they are close we will block them as long as we arent close
     # if neither are close we will go for any random possible winning spot
+def minimax(board, player, bot, turn):
+    new_board = copy.deepcopy(board)
+    if win(board, player, bot) == player:
+        return -10, None
+    elif win(board, player, bot) == bot:
+        return 10, None
+    
+    
+    best_points = float("inf") if turn % 2 == 1 else float("-inf")
+    best_move = None
+    for x in range(3):
+        for y in range(3):
+            if new_board[x][y] == "_":
+                if turn % 2 == 1:
+                    new_board[x][y] = bot
+                else:
+                    new_board[x][y] = player
+                prettyBoard(new_board)
+                score, _ = minimax(new_board, player, bot, turn+1)
+                new_board[x][y] = "_"
+                print(f"Evaluating move ({x}, {y}) for {'bot' if turn % 2 == 1 else 'player'}: score = {score}")
+                if turn % 2 == 1:
+                    if score >= best_points:
+                        best_points = score
+                        best_move = (x, y)
+                else:
+                    if score <= best_points:
+                        best_points = score
+                        best_move = (x, y)
+                print(best_points)
+    print(f"Best move for {'bot' if turn % 2 == 1 else 'player'}: {best_move} with score = {best_points}")
+    return best_points, best_move
+print(minimax(initboard, player, bot, 3))
